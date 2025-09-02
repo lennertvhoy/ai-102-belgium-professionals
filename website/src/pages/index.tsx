4f2c027a359d5a5aae54164220ea2ab1dfd82c5c
lennertvhoy <lennertvhoy@gmail.com>
2025-09-02 11:36:16 +0200

    fix: Correct broken link on homepage

--- website/src/pages/index.tsx
+++ website/src/pages/index.tsx
@@ -20,8 +20,8 @@ function HomepageHeader() {
         <div className={styles.buttons}>
           <Link
             className="button button--secondary button--lg"
-            to="/docs/intro">
-            Docusaurus Tutorial - 5min ⏱️
+            to="/docs/Part1_Strategic_Framework/1.1_The_Belgian_Economic_Imperative_for_AI_Adoption">
+            Start the Course
           </Link>
         </div>
       </div>
